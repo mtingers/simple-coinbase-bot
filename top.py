f11853084d26c3d38db7f95aa5d35140a0c35e7e
Matthew Ingersoll <matth@mtingers.com>
2020-12-02 18:01:44 -0800

    Update top

--- top.py
+++ top.py
@@ -33,6 +33,10 @@ def sec2time(sec, n_msec=3):
         return pattern % (h, m, s)
     return ('%d days, ' + pattern) % (d, h, m, s)
 
+def avg(l):
+    if len(l) < 1:
+        return Decimal(0.0)
+    return Decimal(sum(l))/len(l)
 
 def top():
     clear()
@@ -43,6 +47,7 @@ def top():
     recent = []
     cur_time = time.time()
     open_times = []
+    profit_dates = {}
     for f in files:
         data = None
         coin = None
@@ -60,6 +65,10 @@ def top():
             first_status = v['first_status']
             epoch = time.mktime(time.strptime(first_status['created_at'].split('.')[0], '%Y-%m-%dT%H:%M:%S'))
             if v['completed'] and 'sell_order_completed' in v and v['sell_order_completed']:
+                date_only = v['sell_order_completed']['done_at'].split('T')[0]
+                if not date_only in profit_dates:
+                    profit_dates[date_only] = []
+                profit_dates[date_only].append(v['profit_usd'])
                 end_epoch = time.mktime(time.strptime(v['sell_order_completed']['done_at'].split('.')[0], '%Y-%m-%dT%H:%M:%S'))
                 epoch_diff = end_epoch - epoch
                 cur_diff = cur_time - end_epoch
@@ -77,9 +86,9 @@ def top():
                 stats[coin]['open_orders'] += 1
 
     sorted_keys = sorted(stats.keys())
-    print('{:>100}-UTC'.format(str(datetime.now()).split('.')[0]))
+    print('{:>100} UTC'.format(str(datetime.now()).split('.')[0]))
     print('{}{}{}'.format(colors.fg.darkgrey, '-'*105, colors.reset))
-    print('{}{:>7} {}{:>15} {}{:>15} {}{:>15} {}{:>15} {}{:>15} {}{:>15}{}'.format(
+    print('{}{:>8} {}{:>15} {}{:>15} {}{:>15} {}{:>15} {}{:>15} {}{:>15}{}'.format(
         colors.fg.lightcyan, 'Coin',
         colors.fg.green, 'Profits',
         colors.fg.yellow, 'Open',
@@ -99,47 +108,64 @@ def top():
     for key in sorted_keys:
         coin = key
         v = stats[key]
-        print('{}{:>7} {}{:>15} {}{:>15} {}{:>15} {}{:>15} {}{:>15} {}{:>15}{}'.format(
+        print('{}{:>8} {}{:>15} {}{:>15} {}{:>15} {}{:>15} {}{:>15} {}{:>15}{}'.format(
             colors.fg.lightcyan, coin,
             colors.fg.green, '$'+str(round(sum(v['profits']), 2)),
             colors.fg.yellow, v['open_orders'],
             colors.fg.blue, v['done_orders'],
             colors.fg.red, v['error_orders'],
-            colors.fg.orange, sec2time(round(sum(v['epoch_diffs'])/len(v['epoch_diffs']), 2), 0) if v['epoch_diffs'] else 'None',
-            colors.fg.pink, '$'+str(round(sum(stats[coin]['profits'])/len(stats[coin]['profits']), 2)),
+            colors.fg.orange, sec2time(round(avg(v['epoch_diffs']), 2), 0) if v['epoch_diffs'] else 'None',
+            colors.fg.pink, '$'+str(round(avg(stats[coin]['profits']), 2)),
             colors.reset,
         ))
         #if v['epoch_diffs']:
-        agg_epoch.append(round(sum(v['epoch_diffs'])/len(v['epoch_diffs']), 2) if v['epoch_diffs'] else Decimal('0.0'))
-        agg_profits.append(round(sum(stats[coin]['profits'])/len(stats[coin]['profits']), 2))
+        agg_epoch.append(round(avg(v['epoch_diffs']), 2) if v['epoch_diffs'] else Decimal('0.0'))
+        agg_profits.append(round(avg(stats[coin]['profits']), 2))
         total_open_orders += v['open_orders']
         total_done_orders += v['done_orders']
         total_error_orders += v['error_orders']
         total_profits += round(sum(v['profits']), 2)
     print('{}{}{}'.format(colors.fg.darkgrey, '-'*105, colors.reset))
-    print('{}{:>7} {}{:>15} {}{:>15} {}{:>15} {}{:>15} {}{:>15} {}{:>15}{}'.format(
+    print('{}{:>8} {}{:>15} {}{:>15} {}{:>15} {}{:>15} {}{:>15} {}{:>15}{}'.format(
             colors.fg.darkgrey, 'all',
             colors.fg.green, '$'+str(total_profits),
             colors.fg.yellow, total_open_orders,
             colors.fg.blue, total_done_orders,
             colors.fg.red, total_error_orders,
-            colors.fg.orange, sec2time(round(sum(agg_epoch)/len(agg_epoch), 2), 0),
-            colors.fg.pink, '$'+str(round(sum(agg_profits)/len(agg_profits), 2)),
+            colors.fg.orange, sec2time(round(avg(agg_epoch), 2), 0),
+            colors.fg.pink, '$'+str(round(avg(agg_profits), 2)),
             colors.reset,
     ))
 
     print('{}{}{}'.format(colors.fg.darkgrey, '-'*105, colors.reset))
     min_open_time = sec2time(round(min(open_times), 2), 0)
     max_open_time = sec2time(round(max(open_times), 2), 0)
-    avg_open_time = sec2time(round(sum(open_times)/len(open_times), 2), 0)
-    print(colors.fg.lightgrey)
-    print('{:>24} {:>15} {:>15} {:>15}'.format('Open order durations', 'Min', 'Max', 'Avg'))
-    print('{:>24} {:>15} {:>15} {:>15}'.format('', min_open_time, max_open_time, avg_open_time))
+    avg_open_time = sec2time(round(avg(open_times), 2), 0)
+    print('{}{:>24} {:>24} {:>24} {:>24}'.format(colors.fg.lightgrey, 'Open order durations', 'Min', 'Max', 'Avg'))
+    print('{:>24} {:>24} {:>24} {:>24}'.format('', min_open_time, max_open_time, avg_open_time))
 
+    # Last 7 days with profits
+    print('{}{}{}'.format(colors.fg.darkgrey, '-'*105, colors.reset))
+    print('Last 7 days with profits:')
+    sorted_dates = sorted(profit_dates.keys(), reverse=True)
+    x = []
+    y = []
+    for key in sorted_dates[:7]:
+        val = profit_dates[key]
+        date_total = round(sum(val), 2)
+        x.append(key)
+        y.append(date_total)
+        #print(colors.fg.cyan, '    {} {}{:>15}'.format(key, colors.fg.green, '$'+str(date_total)))
+    max_y = max(y)
+    width = 70
+    for i in range(len(y)):
+        key = x[i]
+        yy = y[i]
+        nstars = int((yy/max_y) * width)
+        print(colors.fg.cyan, '    {} {}{:>15} {}{}'.format(key, colors.fg.green, '$'+str(yy), colors.fg.darkgrey, '*'*nstars))
     if recent:
         print('{}{}{}'.format(colors.fg.darkgrey, '-'*105, colors.reset))
-        print(colors.fg.lightgrey)
-        print('Recently completed:', colors.fg.lightblue)
+        print('Recently completed orders:', colors.fg.lightblue)
     for coin, v in recent:
         first_status = v['first_status']
         epoch = time.mktime(time.strptime(first_status['created_at'].split('.')[0], '%Y-%m-%dT%H:%M:%S'))
