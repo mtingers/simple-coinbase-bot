04a52f8fe5c241c8a3894d81b64496cd09b39bb0
Matthew Ingersoll <matth@mtingers.com>
2020-12-05 00:25:14 -0800

    Update simpletop

--- simplecoinbasebot/top.py
+++ simplecoinbasebot/top.py
@@ -1,3 +1,8 @@
+"""
+TODO:
+    1. Refactor code so it's organized (now that I have an idea of what it'll do)
+    2. Add more sorting by changing stat dict to something that's easier to sort
+"""
 import os
 import sys
 import glob
@@ -9,9 +14,11 @@ from os import system, name
 from datetime import timedelta, datetime
 import threading
 import re
+from collections import OrderedDict
+from operator import getitem
 from .color import colors
 from .getch import getch, getchb
-
+from .termsize import get_terminal_size
 os.environ['TZ'] = 'UTC'
 time.tzset()
 
@@ -19,6 +26,7 @@ GETCH_LOCK = threading.Lock()
 g_last_input = None
 g_filter = ''
 g_paused = False
+g_show_orders = False
 
 def clear():
     if name == 'nt':
@@ -30,13 +38,18 @@ def get_input():
     global g_last_input
     global g_paused
     global g_filter
+    global g_show_orders
     while 1:
         with GETCH_LOCK:
             g_last_input = getch()
         if g_last_input == 'q':
             print('exiting...')
             break
-        if g_last_input == 'f':
+        elif g_last_input == 'o':
+            g_show_orders = True
+        elif g_last_input == 's':
+            g_show_orders = False
+        elif g_last_input == 'f':
             with GETCH_LOCK:
                 g_paused = True
                 buf = 'Enter filter regex (e.g. BT.*): '
@@ -84,14 +97,7 @@ def get_input():
                     g_filter = ''
                 g_paused = False
         time.sleep(0.001)
-        """
-        data = input()
-        print(data)
-        g_last_input = data.strip()
-        if g_last_input == 'q':
-            return
-        time.sleep(.25)
-        """
+
 def draw_line(thickness):
     if thickness == 1:
         print('{}{}{}'.format(colors.fg.darkgrey, u'\u2581'*80, colors.reset))
@@ -113,8 +119,13 @@ def sec2time(sec):
     h, m = divmod(m, 60)
     d, h = divmod(h, 24)
     pattern = r'%2dh %2dm %2ds'
+    if h == 0 and m == 0:
+        return r'%2ds' % (s)
+    if h == 0 and d == 0:
+        return r'%2dm %2ds' % (m, s)
     if d == 0:
-        return pattern % (h, m, s)
+        #return pattern % (h, m, s)
+        return r'%2dh %2dm %2ds'  % (h, m, s)
     return ('%dd' + pattern) % (d, h, m, s)
 
 def avg(l):
@@ -122,9 +133,8 @@ def avg(l):
         return Decimal(0.0)
     return Decimal(sum(l))/len(l)
 
-def top():
-    clear()
-    print('')
+
+def show_orders():
     files = glob.glob(sys.argv[1]+'/*.cache')
     if not files:
         print('ERROR: empty directory')
@@ -135,6 +145,76 @@ def top():
     cur_time = time.time()
     open_times = []
     profit_dates = {}
+    print('[{}q{}]{}uit {}[{}f{}]{}ilter {}[{}s{}]{}tats{:>51} UTC'.format(
+        colors.fg.red, colors.reset, colors.fg.lightgrey,
+        colors.reset, colors.fg.pink, colors.reset, colors.fg.lightgrey,
+        colors.reset, colors.fg.green, colors.reset, colors.fg.lightgrey,
+        str(datetime.now()).split('.')[0])
+    )
+    draw_line(1)
+    print('Open orders:')
+    print('{:>17} {:>9} {:>16} {:>16} {:>18}'.format(
+        'Duration', 'Coin', 'Bought', 'Sell-Price', 'Size',
+    ))
+    w, h = get_terminal_size()
+    hn = 0
+    total = 0
+    for f in files:
+        data = None
+        coin = None
+        with open(f, "rb") as fd:
+            data = pickle.load(fd)
+        if not data:
+            continue
+        for order_id in data: #.items():
+            v = data[order_id]
+            data[order_id]['created_at'] = v['first_status']['created_at']
+        sorted_data = OrderedDict(sorted(data.items(), key = lambda x: getitem(x[1], 'created_at'), reverse=True))
+        #sorted_keys = sorted(data.keys(), reverse=True)
+        for order_id, v in sorted_data.items():
+            if not v['first_status']:
+                continue
+            coin = v['first_status']['product_id']
+            if g_filter:
+                if not re.search(g_filter, coin, re.IGNORECASE):
+                    continue
+            if v['completed'] or not v['sell_order']:
+                continue
+            sell = v['sell_order']
+            created_at = time.mktime(time.strptime(sell['created_at'].split('.')[0], '%Y-%m-%dT%H:%M:%S'))
+            duration = cur_time - created_at
+            price = sell['price']
+            size = sell['size']
+            bought_price = round(Decimal(v['last_status']['executed_value']) / Decimal(v['last_status']['filled_size']), 4)
+            if hn+10 < h:
+                print('{:>17} {:>9} {:>16} {:>16} {:>18}'.format(
+                    sec2time(duration),
+                    sell['product_id'],
+                    bought_price, price,
+                    size,
+                ))
+            hn += 1
+    if hn > h:
+        print('{}... truncated ({}/{} displayed) ...{}'.format(colors.fg.red, h, hn, colors.reset))
+
+def top():
+    clear()
+    print('')
+    if g_show_orders:
+        return show_orders()
+
+    files = glob.glob(sys.argv[1]+'/*.cache')
+    if not files:
+        print('ERROR: empty directory')
+        exit(1)
+    stats = {}
+    stats_incomplete = {}
+    recent = []
+    cur_time = time.time()
+    open_times = []
+    profit_dates = {}
+    w, h = get_terminal_size()
+    hn = 0
     for f in files:
         data = None
         coin = None
@@ -151,7 +231,10 @@ def top():
                 if not re.search(g_filter, coin, re.IGNORECASE):
                     continue
             if not coin in stats:
-                stats[coin] = {'epoch_diffs':[], 'profits':[], 'open_orders':0, 'done_orders':0, 'avg_close_time':0.0, 'error_orders':0, }
+                stats[coin] = {
+                    'epoch_diffs':[], 'profits':[], 'profits_total':Decimal('0.0'),
+                    'open_orders':0, 'done_orders':0, 'avg_close_time':0.0, 'error_orders':0,
+                }
             first_status = v['first_status']
             epoch = time.mktime(time.strptime(first_status['created_at'].split('.')[0], '%Y-%m-%dT%H:%M:%S'))
             if v['completed'] and 'sell_order_completed' in v and v['sell_order_completed']:
@@ -167,6 +250,7 @@ def top():
                 profit = v['profit_usd']
                 stats[coin]['epoch_diffs'].append(epoch_diff)
                 stats[coin]['profits'].append(profit)
+                stats[coin]['profits_total'] += profit
                 stats[coin]['done_orders'] += 1
             elif v['completed']:
                 stats[coin]['error_orders'] += 1
@@ -175,10 +259,13 @@ def top():
                 open_times.append(cur_time - start_epoch)
                 stats[coin]['open_orders'] += 1
 
-    sorted_keys = sorted(stats.keys())
-    print('[{}q{}]{}uit {}[{}f{}]{}ilter {:>59} UTC'.format(
+    #sorted_keys = sorted(stats.keys())
+    #sorted_keys = sorted(stats.items(), key=lambda item: item['profits'])
+    sorted_keys = OrderedDict(sorted(stats.items(), key = lambda x: getitem(x[1], 'profits_total'), reverse=True))
+    print('[{}q{}]{}uit {}[{}f{}]{}ilter {}[{}o{}]{}rders{:>50} UTC'.format(
         colors.fg.red, colors.reset, colors.fg.lightgrey,
         colors.reset, colors.fg.pink, colors.reset, colors.fg.lightgrey,
+        colors.reset, colors.fg.blue, colors.reset, colors.fg.lightgrey,
         str(datetime.now()).split('.')[0])
     )
     draw_line(1)
@@ -199,21 +286,23 @@ def top():
     total_error_orders = 0
     agg_epoch = []
     agg_profits = []
-    for key in sorted_keys:
+    for key,v  in sorted_keys.items():
         coin = key
         if not re.search(g_filter, coin, re.IGNORECASE):
             continue
-        v = stats[key]
-        print('{}{:>8} {}{:>13} {}{:>7} {}{:>7} {}{:>7} {}{:>12} {}{:>19}{}'.format(
-            colors.fg.lightcyan, coin,
-            colors.fg.green, '$'+str(round(sum(v['profits']), 2)),
-            colors.fg.yellow, v['open_orders'],
-            colors.fg.blue, v['done_orders'],
-            colors.fg.red, v['error_orders'],
-            colors.fg.pink, '$'+str(round(avg(stats[coin]['profits']), 2)),
-            colors.fg.orange, sec2time(round(avg(v['epoch_diffs']), 2)) if v['epoch_diffs'] else 'None',
-            colors.reset,
-        ))
+        #v = stats[key]
+        if hn+10 < h:
+            print('{}{:>8} {}{:>13} {}{:>7} {}{:>7} {}{:>7} {}{:>12} {}{:>19}{}'.format(
+                colors.fg.lightcyan, coin,
+                colors.fg.green, '$'+str(round(sum(v['profits']), 2)),
+                colors.fg.yellow, v['open_orders'],
+                colors.fg.blue, v['done_orders'],
+                colors.fg.red, v['error_orders'],
+                colors.fg.pink, '$'+str(round(avg(stats[coin]['profits']), 2)),
+                colors.fg.orange, sec2time(round(avg(v['epoch_diffs']), 2)) if v['epoch_diffs'] else 'None',
+                colors.reset,
+            ))
+        hn += 1
         #if v['epoch_diffs']:
         agg_epoch.append(round(avg(v['epoch_diffs']), 2) if v['epoch_diffs'] else Decimal('0.0'))
         agg_profits.append(round(avg(stats[coin]['profits']), 2))
@@ -221,19 +310,23 @@ def top():
         total_done_orders += v['done_orders']
         total_error_orders += v['error_orders']
         total_profits += round(sum(v['profits']), 2)
-    draw_line(1)
-    print('{}{:>8} {}{:>13} {}{:>7} {}{:>7} {}{:>7} {}{:>12} {}{:>19}{}'.format(
-            colors.fg.darkgrey, 'all',
-            colors.fg.green, '$'+str(total_profits),
-            colors.fg.yellow, total_open_orders,
-            colors.fg.blue, total_done_orders,
-            colors.fg.red, total_error_orders,
-            colors.fg.pink, '$'+str(round(avg(agg_profits), 2)),
-            colors.fg.orange, sec2time(round(avg(agg_epoch), 2)),
-            colors.reset,
-    ))
-    print('')
-    draw_line(3)
+    if hn+12 < h:
+        draw_line(1)
+        print('{}{:>8} {}{:>13} {}{:>7} {}{:>7} {}{:>7} {}{:>12} {}{:>19}{}'.format(
+                colors.fg.darkgrey, 'all',
+                colors.fg.green, '$'+str(total_profits),
+                colors.fg.yellow, total_open_orders,
+                colors.fg.blue, total_done_orders,
+                colors.fg.red, total_error_orders,
+                colors.fg.pink, '$'+str(round(avg(agg_profits), 2)),
+                colors.fg.orange, sec2time(round(avg(agg_epoch), 2)),
+                colors.reset,
+        ))
+        print('')
+        hn += 3
+    if hn+10 < h:
+        draw_line(3)
+    hn += 1
     if open_times:
         min_open_time = sec2time(round(min(open_times), 2))
         max_open_time = sec2time(round(max(open_times), 2))
@@ -242,11 +335,14 @@ def top():
         min_open_time = Decimal('0.0')
         max_open_time = Decimal('0.0')
         avg_open_time = Decimal('0.0')
-    print('{}{:>16} {:>16} {:>16} {:>16}'.format(colors.fg.lightgrey, 'Open order times', 'Min', 'Max', 'Avg'))
-    print('{}{:>16} {:>16} {:>16} {:>16}'.format(colors.fg.lightred, ' ', min_open_time, max_open_time, avg_open_time))
+    if hn+12 < h:
+        print('{}{:>16} {:>16} {:>16} {:>16}'.format(colors.fg.lightgrey, 'Open order times', 'Min', 'Max', 'Avg'))
+        print('{}{:>16} {:>16} {:>16} {:>16}'.format(colors.fg.lightred, ' ', min_open_time, max_open_time, avg_open_time))
+    hn += 2
 
     # Last 7 days with profits
     #print('{}{:>26}'.format(colors.fg.lightgrey, 'Last 7 days profits'))
+    sorted_dates_val = OrderedDict(sorted(profit_dates.items(), key = lambda x: x[1], reverse=True))
     sorted_dates = sorted(profit_dates.keys(), reverse=True)
     x = []
     y = []
@@ -259,7 +355,9 @@ def top():
         y.append(date_total)
         #print(colors.fg.cyan, '    {} {}{:>15}'.format(key, colors.fg.green, '$'+str(date_total)))
 
-    draw_line(3)
+    if hn+10 < h:
+        draw_line(3)
+    hn += 1
     if y:
         max_y = max(y)
         width = 50
@@ -267,12 +365,24 @@ def top():
             key = x[i]
             yy = y[i]
             nstars = int((yy/max_y) * width)
-            print('{}{}{}{:>11} {}{}'.format(colors.fg.cyan, key, colors.fg.green, '$'+str(yy), colors.fg.darkgrey, '*'*nstars))
+            if hn+10 < h:
+                print('{}{}{}{:>11} {}{}'.format(colors.fg.cyan, key, colors.fg.green, '$'+str(yy), colors.fg.darkgrey, '*'*nstars))
+            hn += 1
     if recent:
-        draw_line(3)
-        print('{}{}'.format(colors.fg.lightgrey, 'Recently completed orders'), colors.fg.blue)
+        if hn+10 < h:
+            draw_line(3)
+            print('{}{}'.format(colors.fg.lightgrey, 'Recently completed orders'), colors.fg.blue)
+        hn += 2
         #print('Recently completed orders:', colors.fg.lightblue)
         print('    {:>8} {:>11} {:>17} {:>19}'.format('Coin', 'Profit', 'Duration', 'Completed'), colors.fg.green)
+        # bubble sort, why not
+        for i in range(len(recent)-1):
+            for j in range(len(recent)-i-1):
+                if recent[j][1]['sell_order_completed']['done_at'] < recent[j+1][1]['sell_order_completed']['done_at']:
+                    tmp = recent[j+1]
+                    recent[j+1] = recent[j]
+                    recent[j] = tmp
+
         for coin, v in recent:
             if not re.search(g_filter, coin, re.IGNORECASE):
                 continue
@@ -282,9 +392,11 @@ def top():
             epoch_diff = end_epoch - epoch
             cur_diff = cur_time - end_epoch
             profit = round(v['profit_usd'], 2)
-            print('    {:>8} {:>11} {:>17} {:>19}'.format(
-                coin, '$'+str(profit), sec2time(epoch_diff), str(sec2time(cur_diff))+' ago')
-            )
+            if hn+10 < h:
+                print('    {:>8} {:>11} {:>17} {:>19}'.format(
+                    coin, '$'+str(profit), sec2time(epoch_diff), str(sec2time(cur_diff))+' ago')
+                )
+            hn += 1
     print(colors.reset)
 
 def usage():
@@ -295,6 +407,7 @@ def main():
     global g_last_input
     global g_paused
     global g_filter
+    global g_show_orders
     if len(sys.argv) != 2:
         usage()
     files = glob.glob(sys.argv[1]+'/*.cache')
@@ -304,6 +417,7 @@ def main():
     input_thread = threading.Thread(target=get_input)
     input_thread.start()
 
+    sleep_time = 1.33
     running = True
     while running:
         while g_paused:
@@ -313,7 +427,13 @@ def main():
                 running = False
                 break
             top()
-        time.sleep(1.25)
+            if g_filter:
+                print('{}{}filter: {}{}'.format(colors.bg.blue, colors.fg.lightgrey, g_filter, colors.reset))
+            if g_show_orders:
+                sleep_time = 2.11
+            else:
+                sleep_time = 1.33
+        time.sleep(sleep_time)
 
 if __name__ == '__main__':
     main()
